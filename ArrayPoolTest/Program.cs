243e79ff4d2d4044b056e4ff5bfd84a9413ffa88
Mikhail Flenov <flenov@gmail.com>
2024-07-16 11:25:41 +0300

    add ArrayPoolTest

---
+++ ArrayPoolTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+BenchmarkRunner.Run<Tests>();
+
+[MemoryDiagnoser]
+public class Tests {
+    int size = 1000000;
+    
+    [Benchmark]
+    public void TestArrayPool()
+    {
+        var pool = System.Buffers.ArrayPool<int>.Create();
+        var array = pool.Rent(size);
+        for (int i = 0; i < size; i++)
+            array[i] = i;
+        pool.Return(array, true);
+    }
+
+    [Benchmark]
+    public void TestList()
+    {
+        var list = new List<int>(size);
+        for (int i = 0; i < size; i++)
+            list.Add(i);
+    }
+}
